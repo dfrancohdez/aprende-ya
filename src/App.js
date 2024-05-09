f3e1bb8e446566c5538b78274c09e2655e463e79
dfrancohdez <dfrancohdez@gmail.com>
2024-05-09 10:29:50 -0600

    login signup firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-"use client"
-import logo from './logo.svg';
-import './css/app.css';
-import {NavBar} from './components/NavBar'
-import {Banner} from './components/Banner'
-import {Skills} from './components/Skills'
-import {Contact} from './components/Contact'
-import {Footer} from './components/Footer'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Projects} from './components/Projects'
+import './css/app.css';
+import "react-toastify/dist/ReactToastify.css";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+
+import Login from "./components/auth/login";
+import SignUp from "./components/auth/signup";
+import GetStart from "./Start";
+import Profile from "./components/auth/profile";
+import { ToastContainer } from "react-toastify";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { auth } from "./components/auth/firebase";
+
+
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
-  )
+    const [user, setUser] = useState();
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            setUser(user);
+        });
+    });
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path="/"
+                    element={user ? <Navigate to="/profile" /> : <GetStart />}
+                />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+            <ToastContainer />
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,26 +3,9 @@ import { useState } from "react"
 import contactImg from "../assets/img/contact-img.svg"
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
-export const Contact = () => {
-    const initialDetails = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        message: ''
-    }
-    const [formDetails, setFormDetail] = useState(initialDetails)
-    const [buttonText, setButtonText] = useState('Send')
-    const [status, setStatus] = useState({})
-    const onFormUpdate = (category, value) => {
-        setFormDetail(prev => {
-            return {
-                ...prev,
-                [category]: value
-
-            }
-        })
-    }
+export const Form = (props) => {
+  const [status,setStatus]=useState('')
+    /*//handleSubmit,formDetails,onFormUpdate,buttonText
     const handleSubmit = async(e)=>{
         e.preventDefault()
         setButtonText("Sending...")
@@ -41,7 +24,7 @@ export const Contact = () => {
         }else{
             setStatus({seccess:false,message:'Something went wrong, please try again later'})
         }
-    }
+    }*/
 
     return (
         <section className="contact" id="connect">
@@ -59,23 +42,23 @@ export const Contact = () => {
                   {({ isVisible }) =>
                     <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                     <h2>Get In Touch</h2>
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={props.handleSubmit}>
                       <Row>
                         <Col size={12} sm={6} className="px-1">
-                          <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                          <input name="firstName" type="text" value={props.firstName} placeholder="First Name" onChange={ props.onFormUpdate} />
                         </Col>
                         <Col size={12} sm={6} className="px-1">
-                          <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                          <input name="lastName"type="text" value={props.lasttName} placeholder="Last Name" onChange={props.onFormUpdate}/>
                         </Col>
                         <Col size={12} sm={6} className="px-1">
-                          <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                          <input name="email"type="email" value={props.email} placeholder="Email Address" onChange={props.onFormUpdate} />
                         </Col>
                         <Col size={12} sm={6} className="px-1">
-                          <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                          <input name="phone"type="tel" value={props.phone} placeholder="Phone No." onChange={props.onFormUpdate}/>
                         </Col>
                         <Col size={12} className="px-1">
-                          <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                          <button type="submit"><span>{buttonText}</span></button>
+                          <textarea name="message"rows="6" value={props.message} placeholder="Message" onChange={props.onFormUpdate}></textarea>
+                          <button type="submit"><span>{props.buttonText}</span></button>
                         </Col>
                         {
                           status.message &&
