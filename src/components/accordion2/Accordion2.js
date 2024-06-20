245bacb2153c862cd811007cbf1f001a40afbdc7
DrakyPym <darkericorbit.020802@gmail.com>
2024-06-20 01:30:01 -0600

    Arreglo de bug

--- src/components/accordion2/Accordion2.js
+++ src/components/accordion2/Accordion2.js
@@ -9,17 +9,17 @@ export const Accordion2 = ({ title, content, tiempo , img}) => {
     };
 
     return (
-        <div className='accordion'>
-            <div className='accordion-title' onClick={toggleAccordion}>
+        <div className='accordion2'>
+            <div className='accordion2-title' onClick={toggleAccordion}>
                 <h4>
-                    <span className={`accordion-icon ${isOpen ? 'open' : ''}`}>
+                    <span className={`accordion2-icon ${isOpen ? 'open' : ''}`}>
                         {isOpen ? '▼' : '▼'}
                     </span>
                     <span>{title}</span>
                     <span>{tiempo}min</span>
                 </h4>
             </div>
-            <div className={`accordion-content ${isOpen ? 'open' : ''}`}>
+            <div className={`accordion2-content ${isOpen ? 'open' : ''}`}>
                 <div className='izq'><img src={img} className='imagen'/></div>
                 <div className='der'>{content}</div>
             </div>
