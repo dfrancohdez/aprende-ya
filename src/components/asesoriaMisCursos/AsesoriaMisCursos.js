27e463b7ba6cbb2939c29d122ea87f220e287d71
DrakyPym <darkericorbit.020802@gmail.com>
2024-06-23 03:23:15 -0600

    Back AsesoriaMisCursos casi terminado

--- src/components/asesoriaMisCursos/AsesoriaMisCursos.js
+++ src/components/asesoriaMisCursos/AsesoriaMisCursos.js
@@ -1,35 +1,41 @@
-import React, { useState } from 'react'
-import portada from '../../assets/img/portada.jpg'
+import React, { useState } from 'react';
+import portada from '../../assets/img/portada.jpg';
 
-import './_asesoriaMisCursos.scss'
+import './_asesoriaMisCursos.scss';
 
-import { Blocks } from 'react-loader-spinner'
-import Rating from '@mui/material/Rating';
-import StarIcon from '@mui/icons-material/Star';
-const Asesoria = ({ nombreCurso, nombre, img,fijar }) => {
-    const [cargando,setImg]=useState(true);
+import { Blocks } from 'react-loader-spinner';
+
+const Asesoria = ({ nombreCurso, nombre, img, fijar }) => {
+    const [cargando, setImg] = useState(true);
+
+    const handleClick = () => {
+        window.location.href = '/visualizarCurso';
+    };
+
     return (
-        
-        <div className='asesoria'>
-           
-            <div className={fijar?'fijar':""}>
+        <div className='asesoria' onClick={handleClick}>
+            <div className={fijar ? 'fijar' : ""}>
                 <div className='centrar-loader'>
-                    <img onLoad={()=>setImg(false)} src={!img ? portada:img}/>
-                    {cargando&&<div className='loader-img'><Blocks
-                        height="80"
-                        width="80"
-                        color="green"
-                        ariaLabel="blocks-loading"
-                        wrapperStyle={{}}
-                        wrapperClass="blocks-wrapper"
-                        visible={true}
-                    /></div>}
+                    <img onLoad={() => setImg(false)} src={!img ? portada : img} />
+                    {cargando && (
+                        <div className='loader-img'>
+                            <Blocks
+                                height="80"
+                                width="80"
+                                color="green"
+                                ariaLabel="blocks-loading"
+                                wrapperStyle={{}}
+                                wrapperClass="blocks-wrapper"
+                                visible={true}
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
             <h6 className='bold'>{nombreCurso}</h6>
             <h6 className='regular'>{nombre}</h6>
-        
         </div>
-    )
-}
-export default Asesoria
+    );
+};
+
+export default Asesoria;
