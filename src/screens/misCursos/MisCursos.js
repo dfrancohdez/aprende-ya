47e210ba32f27b7f29f190b5eaca1bf6cd9216a2
DrakyPym <darkericorbit.020802@gmail.com>
2024-06-23 02:53:45 -0600

    Back misCursos terminado

--- src/screens/misCursos/MisCursos.js
+++ src/screens/misCursos/MisCursos.js
@@ -1,80 +1,99 @@
 import { NavBar } from '../../components/navBar/NavBar'
 import { Footer } from '../../components/footer/Footer'
 import { Sidebar } from '../../components/navBar/sidebar/Sidebar'
+import { auth, db } from "../../components/auth/firebase";
+import { doc, getDoc, collection } from "firebase/firestore";
 import TituloBar from '../../components/tituloBar/TituloBar'
 
-
 import './_misCursos.scss'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import React, { Suspense, lazy } from 'react'
 const AsesoriaMisCursos = lazy(() => import('../../components/asesoriaMisCursos/AsesoriaMisCursos'))
 
 function MisCursos() {
-    const [sidebar, toggleSideBar] = useState(false)
+    const [sidebar, toggleSideBar] = useState(false);
+    const [cursosComprados, setCursosComprados] = useState([]);
+
+    const user = auth.currentUser;
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const userDocRef = doc(db, 'Users', user.uid);
+                const userDocSnap = await getDoc(userDocRef);
+
+                if (userDocSnap.exists()) {
+                    const userData = userDocSnap.data();
+                    const cursosCompradosData = userData.cursosComprados || [];
+
+                    const cursosCompradosInfo = await Promise.all(cursosCompradosData.map(async function (curso) {
+                        const partes = curso.split('-');
+                        const parte1 = partes[0]; //Id
+                        const parte2 = partes[1]; //curso
+
+                        const asesoriasDocRef = doc(db, 'Asesorias', parte1);
+                        const cursosCollectionRef = collection(asesoriasDocRef, 'Cursos');
+                        const cursoDocRef = doc(cursosCollectionRef, parte2);
+                        const cursoDocSnapshot = await getDoc(cursoDocRef);
+                        if (cursoDocSnapshot.exists()) {
+                            const cursoData = cursoDocSnapshot.data();
+                            return cursoData;
+                        } else {
+                            console.log('El documento curso no existe');
+                            return null;
+                        }
+                    }));
+
+                    setCursosComprados(cursosCompradosInfo); // Guardar los cursos comprados en el estado local
+                } else {
+                    console.log('El documento del usuario no existe');
+                }
+            } catch (error) {
+                console.error('Error al obtener el documento del usuario:', error);
+            }
+        };
+
+        if (user) {
+            fetchUserData();
+        }
+        
+
+    }, [user]);
 
     const handleToggleSidebar = () => {
-        toggleSideBar(prev => !prev)
-    }
+        toggleSideBar(prev => !prev);
+    };
 
     return (
-        <div className='cursosScreen'>
+        <div className='misCursosScreen'>
             <NavBar type1={true} type2={false} handleToggleSidebar={handleToggleSidebar} />
             <div className={sidebar ? "blur" : ""}></div>
-            
+
             <div>
                 <Sidebar sidebar={sidebar} type1={true} type2={false} />
                 <div className={sidebar ? "blur" : ""}></div>
                 <div className='tituloBar'>
-                    <TituloBar/>
+                    <TituloBar />
                 </div>
-            </div> 
-            
-            
-            <div className='cursosScreen-container'>
-                <Suspense fallback={<div></div>}>
-                    <AsesoriaMisCursos
-                        fijar={true}
-                        nombreCurso={'Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL'}
-                        nombre={'Juan Pablo De la torre Valdez'}
-                        //img={prev.img}
-                    />
-                </Suspense>
-                <Suspense fallback={<div></div>}>
-                    <AsesoriaMisCursos
-                        fijar={true}
-                        nombreCurso={'Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL'}
-                        nombre={'Juan Pablo De la torre Valdez'}
-                        //img={prev.img}
-                    />
-                </Suspense>
-                <Suspense fallback={<div></div>}>
-                    <AsesoriaMisCursos
-                        fijar={true}
-                        nombreCurso={'Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL'}
-                        nombre={'Juan Pablo De la torre Valdez'}
-                        //img={prev.img}
-                    />
-                </Suspense>
-                <Suspense fallback={<div></div>}>
-                    <AsesoriaMisCursos
-                        fijar={true}
-                        nombreCurso={'Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL'}
-                        nombre={'Juan Pablo De la torre Valdez'}
-                        //img={prev.img}
-                    />
-                </Suspense>
-                <Suspense fallback={<div></div>}>
-                    <AsesoriaMisCursos
-                        fijar={true}
-                        nombreCurso={'Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL'}
-                        nombre={'Juan Pablo De la torre Valdez'}
-                        //img={prev.img}
-                    />
-                </Suspense>
             </div>
-            
+
+            <div className='misCursosScreen-container'>
+            {cursosComprados.map(curso => (
+                <Suspense key={curso.img} fallback={<div></div>}>
+                    <AsesoriaMisCursos
+                        fijar={true}
+                        nombreCurso={curso.nombreCurso}
+                        nombre={curso.nombre}
+                        img={curso.img}
+                    />
+                </Suspense>
+            ))}
+
+            </div>
+
             <Footer />
         </div>
-    )
+    );
 }
+
 export default MisCursos;
